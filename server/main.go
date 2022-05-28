b2c7d9bbc5718459ea2bb0576279ebe04f181a90
yasu2122yasu <ya2su1shi2@icloud.com>
2022-05-28 19:53:35 +0900

    モジュール対応モードに変更

--- server/main.go
+++ server/main.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-	v := "不動産管理システムプロジェクト"
-	fmt.Println(v)
+	// Echo instance
+	e := echo.New()
+
+	// Routes
+	e.GET("/", hello)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+// Handler
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
 }
