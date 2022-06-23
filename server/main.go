d815b72d127175e202bf70ee5b725b383501845e
yasu2122yasu <ya2su1shi2@icloud.com>
2022-06-23 09:51:35 +0900

    サンプルコードの追加

--- server/main.go
+++ server/main.go
@@ -3,11 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	prices := []int{98, 54, 43, 32, 12}
-	items := []string{"消しゴム", "鉛筆", "生ゴミ", "煙突", "ケース"}
+	prices := []int{32, 32, 32, 132, 432}
+	items := []string{"消しゴム", "物体", "ゴミ", "ノート", "鉛筆"}
+	sum := 0
 
 	for i := 0; i < len(prices); i++ {
-		fmt.Printf("%s: %d円/n", items[i], prices[i])
+		sum += prices[i]
+		fmt.Printf("%s: %d円｜小計： %d円\n", items[i], prices[i], sum)
 	}
-
+	fmt.Printf("総計： %d円¥n", sum)
 }
