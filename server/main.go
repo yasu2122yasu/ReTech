636ab584645140211e89d4cece967c37b045e994
yasu2122yasu <ya2su1shi2@icloud.com>
2022-06-26 00:06:05 +0900

    サンプルコードの追加

--- server/main.go
+++ server/main.go
@@ -3,34 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	scores := [5][3]int{
-		{43, 31, 34}, {32, 65, 75}, {43, 43, 12},
-		{76, 45, 24}, {95, 142, 64},
-	}
+	thenum := 4147
 
-	for i := 0; i < 5; i++ {
-		total := scores[i][0]+scores[i][1]+scores[i][2] > 179
-		balance := scores[i][0] > 59 && scores[i][1] > 59 && scores[i][2] > 59
-		talent := scores[i][0] > 89 || scores[i][1] > 89 || scores[i][2] > 89
-
-		fmt.Printf("受験者%dさんは：\n", i+1)
-		if total {
-			fmt.Println("総得点で合格")
+	fmt.Println(thenum)
+	for n := 0; n < thenum; n++ {
+		switch {
+		case n == 0:
+		case n == 1:
+		case thenum%n == 0:
+			thenum /= n
+			fmt.Printf("を%dで割ると、%d\n", n, thenum)
+		default:
 		}
-		if balance {
-			fmt.Println("すべての課題で合格")
-		}
-		if !total && !balance && talent {
-			fmt.Println("一つだけ優秀")
-		}
-
-		if total && balance {
-			fmt.Println("おめでとうございます。")
-		} else if total || talent {
-			fmt.Println("来週、挑戦できます。")
-		} else {
-			fmt.Println("来年、また挑戦してください。")
-		}
-		fmt.Println()
 	}
 }
