864a91f23b043f6ff157f18442155758703289c5
yasu2122yasu <ya2su1shi2@icloud.com>
2022-06-26 15:20:07 +0900

    サンプルコードの追加

--- server/main.go
+++ server/main.go
@@ -10,8 +10,25 @@ func scoreavg(scores []int) int {
 	return sum / len(scores)
 }
 
+func sbsqr(a int, b int) int {
+	return (a - b) * (a - b)
+}
+
+func avgvar(scores []int, avg int) int {
+	sum := 0
+	for i := 0; i < len(scores); i++ {
+		if avg == 0 {
+			sum += scores[i]
+		} else {
+			sum += sbsqr(scores[i], avg)
+		}
+	}
+	return sum / len(scores)
+}
+
 func main() {
-	mathscores := []int{40, 45, 32, 24, 53, 61, 98}
-	fmt.Printf("%dによる数学試験の結果:\n", len(mathscores))
+	mathscores := []int{43, 54, 42, 76, 86, 15, 75, 73, 62, 76}
+	fmt.Printf("%d名による数学試験の結果：\n", len(mathscores))
 	fmt.Printf("平均点 %d点\n", scoreavg(mathscores))
+	fmt.Printf("分散　 %d(点・点）\n", avgvar(mathscores, avgvar(mathscores, 0)))
 }
