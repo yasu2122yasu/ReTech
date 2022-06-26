50ea2214ec8ad25dc4fdeb3cc57a5ba1faf53eb1
yasu2122yasu <ya2su1shi2@icloud.com>
2022-06-26 17:30:01 +0900

    サンプルコードの追加

--- server/main.go
+++ server/main.go
@@ -2,49 +2,37 @@ package main
 
 import "fmt"
 
-func scoreavg(scores []int) int {
-	sum := 0
-	for i := 0; i < len(scores); i++ {
-		sum += scores[i]
-	}
-	return sum / len(scores)
+func add(a int, b int) (result int, state string) {
+	result = a + b
+	state = "a + b"
+	return
 }
 
-func sbsqr(a int, b int) int {
-	return (a - b) * (a - b)
+func sub(a int, b int) (result int, state string) {
+	result = a - b
+	state = "a - b"
+	return
 }
 
-func avgvar(scores []int, avg int) int {
-	sum := 0
-	for i := 0; i < len(scores); i++ {
-		if avg == 0 {
-			sum += scores[i]
-		}
-	}
-	return sum / len(scores)
+func mul(a int, b int) (result int, state string) {
+	result = a * b
+	state = "a * b"
+	return
 }
 
-func scoremaxmin(scores []int) (max int, min int) {
-	max = scores[0]
-	min = scores[0]
-
-	for i := 1; i < len(scores); i++ {
-		if max < scores[i] {
-			max = scores[i]
-		}
-		if min > scores[i] {
-			min = scores[i]
-		}
-	}
+func div(a int, b int) (result int, state string) {
+	result = a / b
+	state = "a / b"
 	return
 }
 
 func main() {
-	mathscores := []int{40, 89, 43, 25, 65, 39, 12, 64, 53}
-	fmt.Printf("%dによる数学試験の結果:\n", len(mathscores))
-	fmt.Printf("平均点 %d点\n", scoreavg(mathscores))
-	fmt.Printf("分散 %d（点・点)", avgvar(mathscores, avgvar(mathscores, 0)))
-	max, min := scoremaxmin(mathscores)
-	fmt.Printf("最高点 %d\n", max)
-	fmt.Printf("最低点 %d\n", min)
+	oprs := []func(int, int) (int, string){
+		add, sub, mul, div,
+	}
+
+	for i := 0; i < len(oprs); i++ {
+		result, state := oprs[i](18, 6)
+		fmt.Printf("%s = %d\n", state, result)
+	}
 }
